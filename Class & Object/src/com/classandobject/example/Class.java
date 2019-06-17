975ced789d0fdc704351824a8b75ee77b5a45050
admin <admin@DESKTOP-OTU1QTJ>
2019-06-17 19:36:12 +0530

    Project for class & object

---
+++ Class & Object/src/com/classandobject/example/Class.java
@@ -0,0 +1,22 @@
+package com.classandobject.example;
+
+public class Class {
+int id;
+int rollno;
+int age;
+String name;
+public Class() {
+System.out.println("i am default constructor");
+}
+public void display() {
+System.out.println("id"+id);
+System.out.println("rollno"+rollno);
+System.out.println("age"+age);	
+}
+public void getname()
+{
+System.out.println("name"+name);	
+	
+}
+
+}
---
+++ Class & Object/src/com/classandobject/example/Object.java
@@ -0,0 +1,20 @@
+package com.classandobject.example;
+
+public class Object {
+public static void main(String[]args) {
+	Class D=new Class();
+	System.out.println(D.id);
+	System.out.println(D.rollno);
+	System.out.println(D.age);
+	D.getname();
+	Class t=new Class();
+	t.id=50;
+	t.rollno=60;
+	t.age=20;
+	t.name="dibya";
+	System.out.println(t.id);
+	System.out.println(t.rollno);
+	System.out.println(t.age);
+	System.out.println(t.name);
+	}
+}
